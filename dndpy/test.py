5a7b4816ad40cb0fc5c68861bae0084dcc5816b5
mkeyur <keyur.master@gmail.com>
2018-07-21 11:14:40 -0700

    from master diff class definition

--- dndpy/test.py
+++ dndpy/test.py
@@ -1,13 +1,9 @@
-class UniqueChars(object):
+class UniqueCharsInPlace(object):
 
     def has_unique_chars(self, string):
         if string is None:
             return False
-        chars_set = set()
         for char in string:
-            if char in chars_set:
+            if string.count(char) > 1:
                 return False
-            else:
-                chars_set.add(char)
         return True
-
