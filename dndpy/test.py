baaf187024d4bda11b84d82268051e9f6d799b30
mkeyur <keyur.master@gmail.com>
2018-07-21 10:29:08 -0700

    from master added print

--- dndpy/test.py
+++ dndpy/test.py
@@ -2,5 +2,6 @@ class UniqueCharsSet(object):
 
     def has_unique_chars(self, string):
         if string is None:
+            print string
             return False
         return len(set(string)) == len(string)
