230ceb37ae38a92f561e6816a3f2131bb83c1817
mkeyur <keyur.master@gmail.com>
2018-07-21 10:43:05 -0700

    from master diff func definition for v1 class

--- dndpy/test.py
+++ dndpy/test.py
@@ -1,10 +1,13 @@
-class UniqueCharsSet(object):
+class UniqueChars(object):
 
     def has_unique_chars(self, string):
         if string is None:
-            print string
             return False
-        return len(set(string)) == len(string)
+        chars_set = set()
+        for char in string:
+            if char in chars_set:
+                return False
+            else:
+                chars_set.add(char)
+        return True
 
-if __name__ == '__main__':
-    unique_chars_set = UniqueCharsSet()
