9de4510aef016729e1c81ea05dd6d017a9d4198c
mkeyur <keyur.master@gmail.com>
2018-07-21 11:41:25 -0700

    master revert to 1 definition of UniqueCharsSet

--- dndpy/test.py
+++ dndpy/test.py
@@ -1,13 +1,6 @@
-class UniqueChars(object):
+class UniqueCharsSet(object):
 
     def has_unique_chars(self, string):
         if string is None:
             return False
-        chars_set = set()
-        for char in string:
-            if char in chars_set:
-                return False
-            else:
-                chars_set.add(char)
-        return True
-
+        return len(set(string)) == len(string)
